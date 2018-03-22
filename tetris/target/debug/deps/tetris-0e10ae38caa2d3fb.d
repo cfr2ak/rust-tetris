c182ce279117a9b98bd81629f21280eda6912b68
Yewken <cfr2ak@gmail.com>
2018-03-22 15:02:03 +0900

    uploading

---
+++ tetris/target/debug/deps/tetris-0e10ae38caa2d3fb.d
@@ -0,0 +1,5 @@
+/home/yiyi/projects/hokudai/rust-tetris/tetris/target/debug/deps/tetris-0e10ae38caa2d3fb: src/main.rs
+
+/home/yiyi/projects/hokudai/rust-tetris/tetris/target/debug/deps/tetris-0e10ae38caa2d3fb.d: src/main.rs
+
+src/main.rs:
---
+++ tetris/target/debug/tetris.d
@@ -0,0 +1,1 @@
+/home/yiyi/projects/hokudai/rust-tetris/tetris/target/debug/tetris: /home/yiyi/projects/hokudai/rust-tetris/tetris/src/main.rs
